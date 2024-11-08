ced4884ac5c895e34db1a533e03b6c43b73464f5
Felix A. Taveras <felixataveras1@gmail.com>
2024-11-08 09:51:55 -0500

    added columns

--- src/lib/components/Tutorials/Tutorials.svelte
+++ src/lib/components/Tutorials/Tutorials.svelte
@@ -1,27 +1,24 @@
-<script>
-import { onMount } from "svelte";
+<script lang="ts">
+    import { onMount } from "svelte";
 
-// import env from "env";
+    const Tutorials_api: string = import.meta.env.VITE_TUTORIAL_API;
 
-const Tutorials_api = import.meta.env.VITE_TUTORIAL_API;
+    let items = [];
+    let selectedItem = null;
 
-let items = [];
-let selectedItem = null;
-
-onMount(async () => {
-    try{
-        const response = await fetch(Tutorials_api)
-        if (response.ok){
-            items = await response.json();
+    // fetching data from api
+    onMount(async () => {
+        try {
+            const response = await fetch(Tutorials_api);
+            if (response.ok) {
+                items = await response.json();
             } else {
-                console.error('Failed to fetch items');
+                console.error("Failed to fetch items");
             }
-    } catch (error) {
-        console.error('Failed to fetch items', error);
-    }
-});
-
-
+        } catch (error) {
+            console.error("Failed to fetch items", error);
+        }
+    });
 
     function selectItem(item) {
         selectedItem = item;
@@ -31,21 +28,51 @@ onMount(async () => {
         selectedItem = null;
     }
 </script>
-<main>
 
+<main class="container">
     {#if !selectedItem}
-    <div>
-        <h2>Practice Your Python Skills</h2>
-        
-        <ul>
-            {#each items as item}
-            <li>
-                <button on:click={() => selectItem(item)}>
-                    {item.title}</button
-                    >
-                </li>
+        <div class="tutorial-list">
+            <ul class="item-list">
+                {#each items as item (item.id)}
+                    <li class="item">
+                        <button
+                            on:click={() => selectItem(item)}
+                            class="item-button"
+                        >
+                            {item.title}</button
+                        >
+                    </li>
                 {/each}
             </ul>
         </div>
-        {/if}
-    </main>
+    {:else}
+        <div class="details">
+            <button on:click={goBack} class="back-button">Go back</button>
+            <h3>{selectedItem.title}</h3>
+            <p>{selectedItem.description}</p>
+            <p>{selectedItem.difficulty}</p>
+        </div>
+    {/if}
+</main>
+
+<style>
+    .container {
+        /* display: flex;
+        flex-direction: column;
+        align-items: center; */
+        gap: 1rem;
+        background-color: #1c1c22;
+        height: 100vh;
+    }
+    .item-list {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 16px;
+    }
+
+    @media (min-width: 768px) {
+        .item-list {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
+</style>
